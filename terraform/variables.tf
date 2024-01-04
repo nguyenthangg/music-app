c24688a0bd20fb3a5caf1f42fe0ffcda70334449
ThagNguyen <nhthang1000@gmail.com>
2024-01-04 15:02:27 +0700

    add action create ec2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,13 @@ variable "aws_region" {
   description = "AWS region"
   default     = "us-west-2"  # Replace with your desired region
 }
+
+variable "aws_ami" {
+  description = "AMI for EC2 instance"
+  default     = "ami-008fe2fc65df48dac"  # Replace with your desired AMI ID
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"  # Change instance type if needed
+}
