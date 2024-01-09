3aff4ce1792699d8004a5b766277186680079d84
ThagNguyen <nhthang1000@gmail.com>
2024-01-09 10:35:00 +0700

    add action create lambda v3

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "file-store-file-tfstate"
+    key            = "staging/statefile.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+  }
+}
