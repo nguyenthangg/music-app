e91237729297a0b4519c4d2039992dc66d4ffc0c
ThagNguyen <nhthang1000@gmail.com>
2024-02-20 15:29:46 +0700

    feature ec2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,13 +2,3 @@ variable "aws_region" {
   description = "AWS region"
   default     = "us-west-2"  # Replace with your desired region
 }
-
-variable "public_key" {
-  
-}
-variable "private_key" {
-  
-}
-variable "key_name" {
-  
-}
