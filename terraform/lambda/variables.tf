58aefccab9dc5f11d90f98d3e329a16cdf123d58
ThagNguyen <nhthang1000@gmail.com>
2024-02-21 16:05:10 +0700

    Hot fix for lambda

--- terraform/lambda/variables.tf
+++ terraform/lambda/variables.tf
@@ -1,6 +1,6 @@
 
 variable "lambda_policy" {
   description = "lambda role"
-  default     = ["arn:aws:iam::aws:policy/CloudWatchFullAccess","arn:aws:iam::aws:policy/AmazonS3FullAccess","arn:aws:iam::aws:policy/AmazonAPIGatewayInvokeFullAccess"]  
+  default     = ["arn:aws:iam::aws:policy/CloudWatchFullAccess","arn:aws:iam::aws:policy/AmazonS3FullAccess","arn:aws:iam::aws:policy/AmazonAPIGatewayInvokeFullAccess","arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"]  
   # Replace with your desired AMI ID
 }
